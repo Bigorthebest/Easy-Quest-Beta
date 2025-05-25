0a63747b94e86a5adef5ade83360f234c4f2d8ef
Sami <samiaminehbabou@gmail.com>
2025-05-25 19:49:31 +0200

    ajout bouton modifier

--- addons/questbeta/interface.gd
+++ addons/questbeta/interface.gd
@@ -2,6 +2,7 @@
 extends Control
 
 var fichier = "user://bdd.json"
+var quete_en_modification = null  # Nouvelle variable pour tracker la quête en cours de modification
 
 func in_list(item_list: ItemList, valeur: String) -> bool:
 	for i in item_list.get_item_count():
@@ -27,16 +28,20 @@ func reloadQuete(fichier):
 				
 				$ItemList.add_item(titre + statut)
 				
+				# Changer la couleur selon le statut
 				var item_index = $ItemList.get_item_count() - 1
 				if not active:
 					$ItemList.set_item_custom_fg_color(item_index, Color.GRAY)
 		else:
 			push_warning("Le fichier existe mais ne contient pas un dictionnaire valide.")
 
-# afficher les détails d'une quête  (sous la liste)je pense qu'il faudrait en faire une autre fenêtre mais bon
+# afficher les détails d'une quête (sous la liste)je pense qu'il faudrait en faire une autre fenêtre mais bon
 func _on_item_list_item_selected(index: int) -> void:
 	var nom_quete = $ItemList.get_item_text(index).split(" [")[0]  # Enlever le statut
 	afficher_details_quete(nom_quete)
+	
+	# activer le bouton modifier quand une quête est sélectionnée
+	$ButtonModifier.disabled = false
 
 func afficher_details_quete(nom_quete: String):
 	if FileAccess.file_exists(fichier):
@@ -49,7 +54,7 @@ func afficher_details_quete(nom_quete: String):
 			for quete in parse_result:
 				if parse_result[quete]["Titre"] == nom_quete:
 					var quete_data = parse_result[quete]
-			
+					
 					if has_node("LabelDescription"):
 						$LabelDescription.text = "Description: " + quete_data.get("Description", "Aucune")
 					if has_node("LabelRecompense"):
@@ -69,28 +74,79 @@ func afficher_details_quete(nom_quete: String):
 					print("========================")
 					break
 
+# charger une quête dans la fenêtre de modification
+func charger_quete_pour_modification(nom_quete: String):
+	if FileAccess.file_exists(fichier):
+		var file = FileAccess.open(fichier, FileAccess.ModeFlags.READ)
+		var contenu = file.get_as_text()
+		file.close()
+		
+		var parse_result = JSON.parse_string(contenu)
+		if typeof(parse_result) == TYPE_DICTIONARY:
+			for quete in parse_result:
+				if parse_result[quete]["Titre"] == nom_quete:
+					var quete_data = parse_result[quete]
+					quete_en_modification = nom_quete
+					
+					# remplir la fenêtre avec les données existantes
+					$Window/LineEditTitre.text = quete_data["Titre"]
+					$Window/LineEditDescription.text = quete_data["Description"]
+					$Window/CheckBoxActive.button_pressed = quete_data.get("Active", true)
+					$Window/LineEditRecompense.text = quete_data.get("Recompense", "")
+					$Window/LineEditQueteSuivante.text = quete_data.get("QueteSuivante", "")
+					$Window/Label.text = "Menu de modification de quête :"
+					$Window/ButtonValider.text = "Modifier"
+					
+					$Window.show()
+					break
+
 func _ready() -> void:
 	reloadQuete(fichier)
 	# connecter le signal de sélection d'item
 	if not $ItemList.item_selected.is_connected(_on_item_list_item_selected):
 		$ItemList.item_selected.connect(_on_item_list_item_selected)
+	
+	# Désactiver le bouton modifier au démarrage
+	$ButtonModifier.disabled = true
 
 func _process(delta: float) -> void:
 	pass
 
 func _on_button_pressed() -> void:
+	# Réinitialiser la fenêtre pour création
+	quete_en_modification = null
+	$Window/Label.text = "Menu de création de quêtes :"
+	$Window/ButtonValider.text = "Valider"
+	
+	# Vider les champs
+	$Window/LineEditTitre.text = ""
+	$Window/LineEditDescription.text = ""
+	$Window/CheckBoxActive.button_pressed = true
+	$Window/LineEditRecompense.text = ""
+	$Window/LineEditQueteSuivante.text = ""
+	$Window/LabelErreur.text = ""
+	
 	#if ($LineEditTitre.text == ""):
-	#	$LabelErreur.show()
-	#	$LabelErreur.text = "Aucun titre entrée !"
+	# $LabelErreur.show()
+	# $LabelErreur.text = "Aucun titre entrée !"
 	#else :
-	#	print($LineEditTitre.text)
-	#	$LabelErreur.hide()
-	#	$ItemList.add_item($LineEditTitre.text)
-	#	$ItemList.size.y += $LineEditTitre.size.y
-	#	Bdd.set_value($LineEditTitre.text,"Description","Vide")
-	#	Bdd.save("res://bdd_quete.cfg")
-		$Window.show()
+	# print($LineEditTitre.text)
+	# $LabelErreur.hide()
+	# $ItemList.add_item($LineEditTitre.text)
+	# $ItemList.size.y += $LineEditTitre.size.y
+	# Bdd.set_value($LineEditTitre.text,"Description","Vide")
+	# Bdd.save("res://bdd_quete.cfg")
+	
+	$Window.show()
 
+#  fonction pour le bouton modifier
+func _on_button_modifier_pressed() -> void:
+	var selected_index = $ItemList.get_selected_items()
+	if selected_index.size() > 0:
+		var nom_quete = $ItemList.get_item_text(selected_index[0]).split(" [")[0]
+		charger_quete_pour_modification(nom_quete)
+	else:
+		print("Aucune quête sélectionnée")
 
 func _on_button_supr_all_pressed() -> void:
 	#Reset de la liste
@@ -107,7 +163,10 @@ func _on_button_supr_all_pressed() -> void:
 	if has_node("LabelQueteSuivante"):
 		$LabelQueteSuivante.text = "Quête suivante: "
 	
-	#Reset de la BDD 
+	# Désactiver le bouton modifier
+	$ButtonModifier.disabled = true
+	
+	#Reset de la BDD
 	var quetes_vides = {}
 	var json_string = JSON.stringify(quetes_vides)
 	var file = FileAccess.open(fichier, FileAccess.ModeFlags.WRITE)
@@ -120,8 +179,9 @@ func _on_button_supr_all_pressed() -> void:
 
 func _on_window_close_requested() -> void:
 	$Window.hide()
-		#for data in Bdd.get_sections() :
-	#	$ItemList.add_item(data)
-	#	$ItemList.size.y += $LineEditTitre.size.y
-	#	$ButtonSuprAll.position.y += $LineEditTitre.size.y
+	#for data in Bdd.get_sections() :
+	# $ItemList.add_item(data)
+	# $ItemList.size.y += $LineEditTitre.size.y
+	# $ButtonSuprAll.position.y += $LineEditTitre.size.y
+	
 	reloadQuete(fichier)
--- addons/questbeta/script-fenetre.gd
+++ addons/questbeta/script-fenetre.gd
@@ -10,16 +10,38 @@ var premiere_save = {
 
 func ajouter_dictionnaire_au_json(fichier_path: String) -> void:
 	var data = {}
-	var cle = 0 
-	var nouvelle_entrer ={
-			'Titre' : $LineEditTitre.text,
-			'Description' : $LineEditDescription.text,
-			'Active': $CheckBoxActive.button_pressed,  # NOUVEAU
-			'Recompense': $LineEditRecompense.text,    # NOUVEAU
-			'QueteSuivante': $LineEditQueteSuivante.text  # NOUVEAU
-		}
+	var cle = 0
+	
+	var nouvelle_entrer = {
+		'Titre': $LineEditTitre.text,
+		'Description': $LineEditDescription.text,
+		'Active': $CheckBoxActive.button_pressed,  
+		'Recompense': $LineEditRecompense.text,    
+		'QueteSuivante': $LineEditQueteSuivante.text  
+	}
+	
+	# Lire le fichier s'il existe
+	if FileAccess.file_exists(fichier_path):
+		var file = FileAccess.open(fichier_path, FileAccess.ModeFlags.READ)
+		var contenu = file.get_as_text()
+		file.close()
+		var parse_result = JSON.parse_string(contenu)
+		if typeof(parse_result) == TYPE_DICTIONARY:
+			data = parse_result
+			cle = parse_result.size()
+		else:
+			push_warning("Le fichier existe mais ne contient pas un dictionnaire valide. Écrasement.")
+	
+	# Ajouter le nouveau dictionnaire
+	data[cle] = nouvelle_entrer
+	
+	# Réécrire le fichier JSON
+	var file = FileAccess.open(fichier_path, FileAccess.ModeFlags.WRITE)
+	file.store_string(JSON.stringify(data, "\t"))  # "\t" pour indenter joliment
+	file.close()
 
-	# Lire le fichier s’il existe
+# Nouvelle fonction pour modifier une quête existante
+func modifier_quete_dans_json(fichier_path: String, ancien_nom: String) -> void:
 	if FileAccess.file_exists(fichier_path):
 		var file = FileAccess.open(fichier_path, FileAccess.ModeFlags.READ)
 		var contenu = file.get_as_text()
@@ -27,21 +49,23 @@ func ajouter_dictionnaire_au_json(fichier_path: String) -> void:
 		
 		var parse_result = JSON.parse_string(contenu)
 		if typeof(parse_result) == TYPE_DICTIONARY:
-			data = parse_result
-			cle = parse_result.size()
-		else:
-			push_warning("Le fichier existe mais ne contient pas un dictionnaire valide. Écrasement.")
+			# Trouver et modifier la quête
+			for quete_id in parse_result:
+				if parse_result[quete_id]["Titre"] == ancien_nom:
+					# Mettre à jour les données
+					parse_result[quete_id]["Titre"] = $LineEditTitre.text
+					parse_result[quete_id]["Description"] = $LineEditDescription.text
+					parse_result[quete_id]["Active"] = $CheckBoxActive.button_pressed
+					parse_result[quete_id]["Recompense"] = $LineEditRecompense.text
+					parse_result[quete_id]["QueteSuivante"] = $LineEditQueteSuivante.text
+					break
+			
+			# Réécrire le fichier JSON
+			var file_write = FileAccess.open(fichier_path, FileAccess.ModeFlags.WRITE)
+			file_write.store_string(JSON.stringify(parse_result, "\t"))
+			file_write.close()
 
-	# Ajouter le nouveau dictionnaire
-	data[cle] = nouvelle_entrer
-
-	# Réécrire le fichier JSON
-	var file = FileAccess.open(fichier_path, FileAccess.ModeFlags.WRITE)
-	file.store_string(JSON.stringify(data, "\t"))  # "\t" pour indenter joliment
-	file.close()
-
-
-func loadBDD(file) :
+func loadBDD(file):
 	var json_as_text = FileAccess.get_file_as_string(file)
 	var json_as_dict = JSON.parse_string(json_as_text)
 	print(typeof(json_as_dict))
@@ -50,35 +74,33 @@ func loadBDD(file) :
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#On essaye de charger les données d'une configurations existante 
+	#On essaye de charger les données d'une configurations existante
 	move_to_center()
 	print("ready")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-	
+
 func _enter_tree() -> void:
 	print("Enter tree...")
-		# Convertir le dictionnaire en chaîne JSON
+	# Convertir le dictionnaire en chaîne JSON
 	#var json = JSON.new()
 	#var json_string = json.stringify(premiere_save)
-	
 	var tab = loadBDD(jsonfile)
 	print(tab)
-
 	# Écrire la chaîne JSON dans un fichier
 	#var file = FileAccess.open("jsonfile", FileAccess.WRITE)
 	#if file.file_exists(jsonfile) :
-	#	file.store_string(json_string)
-	#	file.close()
-	#else : 
-	#	print("Impossible d'ouvrir le fichier :" + jsonfile)
+	# file.store_string(json_string)
+	# file.close()
+	#else :
+	# print("Impossible d'ouvrir le fichier :" + jsonfile)
 
 func _init() -> void:
 	print("init")
 
-func verifier_quete_existe(nom_quete: String) -> bool: #
+func verifier_quete_existe(nom_quete: String) -> bool:  #
 	if FileAccess.file_exists(jsonfile):
 		var file = FileAccess.open(jsonfile, FileAccess.ModeFlags.READ)
 		var contenu = file.get_as_text()
@@ -109,10 +131,21 @@ func _on_button_valider_pressed() -> void:
 				# return  # bon c'est un peu bête, ça sera jamais utile
 		
 		print("Création de quête : " + $LineEditTitre.text)
-		$LabelErreur.text = "" #pour "cacher" la potentiel erreur 
+		$LabelErreur.text = ""  #pour "cacher" la potentiel erreur
+		
+		# vérifier si on est en mode modification ou création
+		var interface_node = get_parent()
+		if interface_node.quete_en_modification != null:
+			# mode modification
+			modifier_quete_dans_json(jsonfile, interface_node.quete_en_modification)
+			print("Quête modifiée : " + $LineEditTitre.text)
+		else:
+			# mode création
+			ajouter_dictionnaire_au_json(jsonfile)
+			print("Store in json")
+		
 		#$ItemList.add_item($Window/LineEditTitre.text)
 		#$ItemList.size.y += $Window/LineEditTitre.size.y
-		
 		#Bdd.set_value($LineEditTitre.text,$LineEditDescription.text,"Vide")
 		#Bdd.save("res://bdd_quete.cfg")
 		#print("Donner sauvegarder dans la bdd")
@@ -122,11 +155,9 @@ func _on_button_valider_pressed() -> void:
 		#var tab = loadBDD(jsonfile)
 		#print(tab.size())
 		#Etape de stockage
-		
 		#var file = FileAccess.open(jsonfile,FileAccess.ModeFlags.WRITE)
 		#var text_to_json = JSON.stringify(tab.merge(nouvelle_entrer))
 		#file.store_string(text_to_json)
-		ajouter_dictionnaire_au_json(jsonfile)
-		print("Store in json")
+		
 		#file.close()
 		close_requested.emit()
