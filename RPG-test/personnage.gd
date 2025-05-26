c1395dc91ed7526beee6db30e378b5f3b6e4165d
Julien BOUTICOURT <julienbouticourt@gmail.com>
2025-05-26 15:54:15 +0200

    Ajout d'un personnage et d'une scéne principale pour le RPG

---
+++ RPG-test/personnage.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+var vitesse = 50 
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("Haut") :
+		position.y -= vitesse * delta 
+		$CharacterBody2D/AnimatedSprite2D.play()
+	if Input.is_action_pressed("Bas") :
+		position.y += vitesse * delta 
+		$CharacterBody2D/AnimatedSprite2D.play()
+	if Input.is_action_pressed("Gauche") :
+		position.x -= vitesse * delta 
+		$".".scale.x = 1 
+		$CharacterBody2D/AnimatedSprite2D.play()
+	if Input.is_action_pressed("Droite") :
+		position.x += vitesse * delta 
+		$".".scale.x = -1 
+		$CharacterBody2D/AnimatedSprite2D.play()
+	if not Input.is_anything_pressed() :
+		$CharacterBody2D/AnimatedSprite2D.pause()
