631d19a18fcb5207cf83831b1da3a748d4187064
Valise2patate <julienbouticourt@gmail.com>
2025-05-28 01:45:34 +0200

    nouveau perso animé

--- RPG-test/personnage.gd
+++ RPG-test/personnage.gd
@@ -11,17 +11,17 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("Haut") :
 		position.y -= vitesse * delta 
-		$CharacterBody2D/AnimatedSprite2D.play()
+		$CharacterBody2D/AnimatedSprite2D.play("marche")
 	if Input.is_action_pressed("Bas") :
 		position.y += vitesse * delta 
-		$CharacterBody2D/AnimatedSprite2D.play()
+		$CharacterBody2D/AnimatedSprite2D.play("marche")
 	if Input.is_action_pressed("Gauche") :
 		position.x -= vitesse * delta 
 		$".".scale.x = 1 
-		$CharacterBody2D/AnimatedSprite2D.play()
+		$CharacterBody2D/AnimatedSprite2D.play("marche")
 	if Input.is_action_pressed("Droite") :
 		position.x += vitesse * delta 
 		$".".scale.x = -1 
-		$CharacterBody2D/AnimatedSprite2D.play()
+		$CharacterBody2D/AnimatedSprite2D.play("marche")
 	if not Input.is_anything_pressed() :
-		$CharacterBody2D/AnimatedSprite2D.pause()
+		$CharacterBody2D/AnimatedSprite2D.play("idle")
