8a1e362b6d011f9a2b68bd5de621126b4d5fb2b6
Julien BOUTICOURT <julienbouticourt@gmail.com>
2025-05-28 16:30:57 +0200

    Colision des tile + double tileset

--- RPG-test/personnage.gd
+++ RPG-test/personnage.gd
@@ -1,27 +1,28 @@
-extends Node2D
+extends CharacterBody2D
 
-var vitesse = 50 
+var vitesse = 80
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
+	velocity = Vector2.ZERO
 	if Input.is_action_pressed("Haut") :
-		position.y -= vitesse * delta 
-		$CharacterBody2D/AnimatedSprite2D.play("marche")
+		velocity.y -= vitesse 
+		$AnimatedSprite2D.play("marche")
 	if Input.is_action_pressed("Bas") :
-		position.y += vitesse * delta 
-		$CharacterBody2D/AnimatedSprite2D.play("marche")
+		velocity.y += vitesse  
+		$AnimatedSprite2D.play("marche")
 	if Input.is_action_pressed("Gauche") :
-		position.x -= vitesse * delta 
-		$".".scale.x = 1 
-		$CharacterBody2D/AnimatedSprite2D.play("marche")
+		velocity.x -= vitesse 
+		$AnimatedSprite2D.scale.x = 1 
+		$AnimatedSprite2D.play("marche")
 	if Input.is_action_pressed("Droite") :
-		position.x += vitesse * delta 
-		$".".scale.x = -1 
-		$CharacterBody2D/AnimatedSprite2D.play("marche")
+		velocity.x += vitesse 
+		$AnimatedSprite2D.scale.x = -1 
+		$AnimatedSprite2D.play("marche")
 	if not Input.is_anything_pressed() :
-		$CharacterBody2D/AnimatedSprite2D.play("idle")
+		$AnimatedSprite2D.play("idle")
+		
+	move_and_slide()
--- addons/questbeta/FonctionEditor.gd
+++ addons/questbeta/FonctionEditor.gd
@@ -1,29 +1,40 @@
 extends EditorProperty
 
-var property_control = Button.new()
 var icon = load("res://addons/questbeta/icon/icon_quest.png")
 var fenetre = null 
 var scene_fen = preload("res://addons/questbeta/interface-select.tscn")
 var fen = scene_fen.instantiate()
 var noeud_courant 
+#Ajout dans l'inspecteur
+var property_control = Button.new()
+var hbox  = HBoxContainer.new()
+var nom = Label.new()
 
 func set_noeud_courant(noeud):
 	noeud_courant = noeud 
 
 func _init():
-	var hbox = HBoxContainer.new()
-	var label = Label.new()
-	label.text = "Option de quete :"
+	hbox.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	
+	nom.text = "Option de quete :"
+	nom.size_flags_horizontal = Control.SIZE_EXPAND_FILL
 
 	property_control.text = "Lier une Quete"
 	property_control.icon = icon
 	property_control.pressed.connect(_on_button_pressed)
-	
-	hbox.add_child(label)	
+
+	hbox.add_child(nom)
 	hbox.add_child(property_control)
 
 	add_child(hbox)
 	add_focusable(hbox)
+
+func _notification(what):
+	if what == NOTIFICATION_RESIZED:
+		hbox.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+		nom.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+		#hbox.size_flags_horizontal = Vector2(size.x, 0)
+		#hbox.size_flags_horizontal = Control.SIZE_EXPAND_FILL
 
 func _on_button_pressed():
 	print("Liée une quete cliquer")
